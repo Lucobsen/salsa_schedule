942d5bc959ee6b310842f1aac791dbafc4cc2b48
Luke Jacobsen <lukejacobsen@gmail.com>
2023-12-26 18:35:17 +0100

    added festival display

--- app/[month]/page.tsx
+++ app/[month]/page.tsx
@@ -1,5 +1,6 @@
 import { festivals } from "@/data/_data";
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { format } from "date-fns";
 
 interface MonthProps {
   params: {
@@ -18,7 +19,47 @@ const Month = ({ params }: MonthProps) => {
       )}
 
       {monthFestivals &&
-        monthFestivals.map((fest) => <Typography>{fest.name}</Typography>)}
+        monthFestivals.map((fest) => (
+          <Box
+            key={fest.name}
+            sx={{
+              height: "fit-content",
+              backgroundColor: "#1976d2",
+              mb: 4,
+              border: "2px solid #fff",
+              borderRadius: 2,
+              color: "#fff",
+              p: 2,
+              textTransform: "uppercase",
+            }}
+          >
+            <Typography variant="h6">{fest.name}</Typography>
+            <Stack direction="row" spacing={2}>
+              <Stack>
+                <Typography variant="caption">
+                  Location: {fest.location}
+                </Typography>
+                <Stack direction="row" spacing={0.5}>
+                  <Typography variant="caption">Styles:</Typography>
+                  {fest.styles.map((style) => (
+                    <Typography key={`${fest.name}-${style}`} variant="caption">
+                      {style}
+                    </Typography>
+                  ))}
+                </Stack>
+              </Stack>
+
+              <Stack>
+                <Typography variant="caption">
+                  Start Date: {format(fest.startDate, "dd-MM-yyyy")}
+                </Typography>
+                <Typography variant="caption">
+                  End Date: {format(fest.endDate, "dd-MM-yyyy")}
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        ))}
     </Container>
   );
 };
