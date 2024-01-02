76cef4dccb58817077fd663e1dd11764b182cd54
Luke Jacobsen <lukejacobsen@gmail.com>
2024-01-02 22:47:42 +0100

    added hover to items

--- app/components/FestivalItem/FestivalItem.tsx
+++ app/components/FestivalItem/FestivalItem.tsx
@@ -1,6 +1,7 @@
 import { Festival } from "@/app/lib/actions";
-import { Box, Grid, Link, Stack, Typography } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import { format } from "date-fns";
+import Link from "next/link";
 
 const FestivalItem = ({
   styles,
@@ -21,35 +22,46 @@ const FestivalItem = ({
       p: 2,
       textTransform: "uppercase",
       width: "100%",
+
+      "&:hover": {
+        boxShadow: "0 0 10px 2px #fff",
+      },
     }}
   >
-    <Link variant="h6" href={url} target="_blank" color="#fff">
-      {name}
+    <Link
+      href={url}
+      target="_blank"
+      style={{ color: "#fff", textDecoration: "none" }}
+    >
+      <Typography color="inherit" variant="h6">
+        {name}
+      </Typography>
+      <Grid container color="inherit">
+        <Grid item xs={12} sm={6}>
+          <Typography variant="caption">Location: {location}</Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Stack direction="row" spacing={0.5}>
+            <Typography variant="caption">
+              Styles: {styles.join(", ")}
+            </Typography>
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Typography variant="caption">
+            Start Date: {format(startDate, "do MMM yyyy")}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Typography variant="caption">
+            End Date: {format(endDate, "do MMM yyyy")}
+          </Typography>
+        </Grid>
+      </Grid>
     </Link>
-
-    <Grid container>
-      <Grid item xs={12} sm={6}>
-        <Typography variant="caption">Location: {location}</Typography>
-      </Grid>
-
-      <Grid item xs={12} sm={6}>
-        <Stack direction="row" spacing={0.5}>
-          <Typography variant="caption">Styles: {styles.join(", ")}</Typography>
-        </Stack>
-      </Grid>
-
-      <Grid item xs={12} sm={6}>
-        <Typography variant="caption">
-          Start Date: {format(startDate, "do MMM yyyy")}
-        </Typography>
-      </Grid>
-
-      <Grid item xs={12} sm={6}>
-        <Typography variant="caption">
-          End Date: {format(endDate, "do MMM yyyy")}
-        </Typography>
-      </Grid>
-    </Grid>
   </Box>
 );
 
